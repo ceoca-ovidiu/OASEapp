bd74ee5209b3b08bf49a9c6508061719922ec517
enyediervin <ervinfcb@gmail.com>
2022-05-17 20:28:15 +0300

    Login signup on back and encrypting

--- backend/run.js
+++ backend/run.js
@@ -1,28 +1,81 @@
-const io = require('socket.io');
+const bcrypt = require("bcrypt");
+const io = require("socket.io");
+var jfile = require("fs");
 const server = io.listen(8000);
-console.log('Server socket is listening on port 8000');
+console.log("Server socket is listening on port 8000");
 let connectedClients = new Map();
-
 // event fired every time a new client connects
-server.on('connection', socket => {
+server.on("connection", (socket) => {
   console.info(`Client connected [id=${socket.id}]`);
   connectedClients.set(socket.id, socket);
-  console.log(connectedClients.size + ' client/s connected');
+  console.log(connectedClients.size + " client/s connected");
 
   // when socket disconnects, remove it from the map
-  socket.on('disconnect', () => {
+  socket.on("disconnect", () => {
     connectedClients.delete(socket.id);
     console.info(`Client [id=${socket.id}] disconnected`);
-    console.log(connectedClients.size + ' client/s connected');
+    console.log(connectedClients.size + " client/s connected");
   });
 
-  socket.on('chat', payload => {
+  socket.on("chat", (payload) => {
     sendMessageToAllOtherClients(socket, payload);
+  });
+
+  socket.on("signup", (payload) => {
+    let succes = signup(payload);
+    socket.emit("signup-message", succes);
+  });
+
+  socket.on("login", (payload) => {
+    let succes = login(payload);
+    socket.emit("login-message", succes);
   });
 });
 
 function sendMessageToAllOtherClients(sender, message) {
   for (let [key, socket] of connectedClients) {
-    socket.emit('message-from-server', { id: sender.id, message: message });
+    socket.emit("message-from-server", { id: sender.id, message: message });
   }
+}
+
+function signup(message) {
+  username = message.username;
+  password = message.password;
+  exists = false;
+  let data = jfile.readFileSync("database.json", "utf-8");
+  let users = JSON.parse(data);
+  users.forEach((element) => {
+    if (element.username === username) {
+      exists = true;
+    }
+  });
+  if (!exists) {
+    users.push({ username: username, password: bcrypt.hashSync(password, 10) });
+    data = JSON.stringify(users);
+    jfile.writeFileSync("database.json", data, "utf-8");
+    return "OK";
+  } else {
+    return "Exists";
+  }
+}
+
+function login(message) {
+  username = message.username;
+  password = message.password;
+  let data = jfile.readFileSync("database.json", "utf-8");
+  let users = JSON.parse(data);
+  let succes = false;
+  users.forEach((element) => {
+    if (
+      element.username === username &&
+      bcrypt.compare(password, element.password)
+    ) {
+      succes = true;
+      return;
+    }
+  });
+  return {
+    succes: succes,
+    user: username,
+  };
 }
--- frontend/client.js
+++ frontend/client.js
@@ -1,119 +1,118 @@
-let socket = io.connect('localhost:8000');
-
+let socket = io.connect("localhost:8000");
 try {
-  socket.on('connect', function () {
-    document.getElementById('client-id').innerHTML = 'Client id: ' + localStorage.getItem('username');
-    if (localStorage.getItem('username')) {
+  socket.on("connect", function () {
+    document.getElementById("client-id").innerHTML =
+      "Client id: " + localStorage.getItem("username");
+    if (localStorage.getItem("username")) {
       document.getElementById("login-content").style.display = "none";
       document.getElementById("main-content").style.display = "block";
       // if login hide form and show chat
     }
   });
 
-  socket.on('message-from-server', function (entry) {
+  socket.on("message-from-server", function (entry) {
     // console.dir(message);
-    let messages = document.getElementsByClassName('messages')[0];
-    let p = document.createElement('p');
-    p.innerText = socket.id + ': ' + entry.message;
+    let messages = document.getElementsByClassName("messages")[0];
+    let p = document.createElement("p");
+    p.innerText = entry.message;
     messages.appendChild(p);
   });
+
+  socket.on("signup-message", (succes) => {
+    if (succes === "OK") {
+      alert("Signup successfull");
+    } else {
+      alert("Signup failed, user already exists!");
+    }
+  });
+
+  socket.on("login-message", (response) => {
+    if (response.succes) {
+      localStorage.setItem("username", response.user);
+      document.getElementById("login-content").style.display = "none";
+      document.getElementById("main-content").style.display = "block";
+    } else {
+      alert("Wrong username or password");
+    }
+  });
 } catch (err) {
-  alert('ERROR: socket.io encountered a problem:\n\n' + err);
+  alert("ERROR: socket.io encountered a problem:\n\n" + err);
 }
 
-document.getElementById('send').addEventListener('click', sendMessage);
+document.getElementById("send").addEventListener("click", sendMessage);
 
 function sendMessage() {
-  let message = document.getElementById('message').value;
+  let message = document.getElementById("message").value;
   if (!(message === "")) {
-    socket.emit('chat', message);
+    message = localStorage.getItem("username") + ":" + message;
+    socket.emit("chat", message);
     document.getElementById("message").value = "";
   }
 }
 
-document.getElementById('loginButton').addEventListener('click', loginBoy);
+document.getElementById("loginButton").addEventListener("keypress", (evnt) => {
+  if (evnt.key == "Enter") {
+    evnt.preventDefault();
+    document.getElementById("loginButton").click();
+  }
+});
+document.getElementById("loginButton").addEventListener("click", loginBoy);
 
 function loginBoy() {
   // get user (userName and password)
-  let form = document.getElementById('form').getElementsByTagName('input')
+  let form = document.getElementById("form").getElementsByTagName("input");
   let user = {
     username: form.userInput.value,
-    password: form.passInput.value
-  }
+    password: form.passInput.value,
+  };
   // check if user exist
   // get json list and check if user iz there
-  fetch('database.json').then(response => {
-    response.json().then(res => {
-      // res is json users
-      res.forEach((el) => {
-        if (JSON.stringify(user) === JSON.stringify(el)) {
-          // user is there and can login
-          socket.emit('user', user.username);
-          localStorage.setItem('username', user.username);
-        }
-      })
-    });
-  });
-
-
+  socket.emit("login", user);
 }
 
-document.getElementById('register').addEventListener('click', register);
+document.getElementById("register").addEventListener("click", register);
 
 function register() {
-
   // get user from form
-  let username = document.getElementById('form').getElementsByTagName('input').userInput.value;
-  if (username === '') {
-    alert('INTRODUCE USERNAME');
-    return false;
+  let data = document.getElementById("form").getElementsByTagName("input");
+  let username = data.userInput.value;
+  let password = data.passInput.value;
+  if (username === "") {
+    alert("ENTER USERNAME");
+  } else if (password === "") {
+    alert("ENTER PASSWORD");
   }
-  console.log(username);
   // check if user already in db
-  fetch('database.json').then(response => {
-    response.json().then(res => {
-      let exists = false;
-      console.log(res)
-      // res is json users
-      res.forEach((el) => {
-        if (el.username === username) {
-          exists = true;
-        }
-      })
-      if (!exists){
-        // if user not exist add user to json
-      }
-    });
-  });
-
+  socket.emit("signup", { username: username, password: password });
 }
 
-document.getElementById('logout').addEventListener('click', logout);
+document.getElementById("logout").addEventListener("click", logout);
 
 function logout() {
-  console.log('logout')
+  console.log("logout");
   // remove storage item so user is log out
-  localStorage.removeItem('username');
+  localStorage.removeItem("username");
   location.reload();
 }
 
-
-document.getElementById('dark-mode-button').addEventListener('click', enableDarkMode);
+document
+  .getElementById("dark-mode-button")
+  .addEventListener("click", enableDarkMode);
 
 function enableDarkMode() {
-  let text = document.getElementById('dark-mode-button').innerText;
-  if (text === 'Dark Mode') {
-    document.getElementById('dark-mode-button').innerText = "Light Mode";
+  let text = document.getElementById("dark-mode-button").innerText;
+  if (text === "Dark Mode") {
+    document.getElementById("dark-mode-button").innerText = "Light Mode";
     document.body.style.backgroundImage = "url('oi-night.jpg')";
-    document.getElementById('title').style.color = 'white';
-    document.getElementById('client-id').style.color = 'white';
-    document.getElementById('messages').style.backgroundColor = "#495057";
+    document.getElementById("title").style.color = "white";
+    document.getElementById("client-id").style.color = "white";
+    document.getElementById("messages").style.backgroundColor = "#495057";
   } else {
-    document.getElementById('dark-mode-button').innerText = "Dark Mode";
+    document.getElementById("dark-mode-button").innerText = "Dark Mode";
     document.body.style.backgroundImage = "url('oi.jpg')";
-    document.getElementById('title').style.color = 'black';
-    document.getElementById('client-id').style.color = 'black';
-    document.getElementById('messages').style.backgroundColor = "#8EADCC";
+    document.getElementById("title").style.color = "black";
+    document.getElementById("client-id").style.color = "black";
+    document.getElementById("messages").style.backgroundColor = "#8EADCC";
   }
 }
 
